ba7e67d52528c4a06cf9309ada6881691bdd3398
Akshay <akshaykomade012345@gmail.com>
2023-08-31 19:11:32 +0530

    Created Pydbcon

---
+++ databasepython/collegepydbcon.py
@@ -0,0 +1,33 @@
+import mysql.connector
+ 
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd = "",
+    database = "pydbtest"
+)
+print(mydb) #to check The connection is made or not with the database
+
+query = mydb.cursor() #using these you can execute any command on your database
+
+#query.execute("CREATE DATABASE pydbtest") #creating Database
+
+#query.execute("CREATE TABLE emp(id int PRIMARY KEY AUTO_INCREMENT,name varchar(244),education varchar(244))") #creating table using sql command
+
+
+#to Insert any data into database 
+# sql = "INSERT INTO emp(name,education) VALUES(%s,%s)" #specified the columns which you have to enter the data
+
+# val = [   #python cursor object only accepts the values in form of tuples
+#     ("Akshay","BCA"),
+#     ("Harry","IIT"),
+#     ("Aman","CS"),
+#     ("Sharrdha","CS")
+# ]
+
+# query.executemany(sql,val) #to execute many command using single line you have to use executemany instead of execute
+
+# mydb.commit() #to commit the changes sql topic
+
+# print(query.rowcount , "'s Row Inserted") # just for being nice and print how many rows inserted into database
+
---
+++ databasepython/first.py
@@ -0,0 +1,34 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    username="root",  # Changed 'username' to 'user'
+    passwd="",    # You might need to provide your MySQL password here
+    database="pydbtest"
+)
+
+mycursor = mydb.cursor()
+
+val = [
+    ("Akshay","Akshay123"),
+    ("user2","user123"),
+    ("user3","user234")
+       ]    
+
+sql = "SELECT password FROM user WHERE name = 'Akshay'"
+
+mycursor.execute(sql)
+
+result = mycursor.fetchone()
+
+compare = result[0]
+
+
+print(compare)
+name = input("Enter Your Name : ")
+password = input("Enter Your password : ")
+
+if password == compare:
+    print("You Are Logged In")
+else:
+    print("No You cant login")
